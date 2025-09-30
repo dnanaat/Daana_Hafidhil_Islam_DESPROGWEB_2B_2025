c2d8cbc2545161408cd67abaee9c45a766b1d34c
dnanaat <daanahafidhilislam@gmail.com>
2025-09-30 22:12:14 +0700

    Jobsheet 7 Form Processing with PHP & jQuery

---
+++ Jobsheet7/isset.php
@@ -0,0 +1,18 @@
+<?php
+
+echo"Percobaan 1<br>";
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+}
+
+echo"<br><br>Percobaan 2<br>";
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data['nama'])) {
+    echo "Nama: " . $data['nama'];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
